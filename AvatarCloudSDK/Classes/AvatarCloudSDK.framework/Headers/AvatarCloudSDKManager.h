c6d30ae1bc93e25986bce3a1b92389764ee7608e
liguibin <liguibin@shuzijiayuan.com>
2022-07-13 20:48:40 +0800

    fix bundle and framework-resource

--- AvatarCloudSDK/Classes/AvatarCloudSDK.framework/Headers/AvatarCloudSDKManager.h
+++ AvatarCloudSDK/Classes/AvatarCloudSDK.framework/Headers/AvatarCloudSDKManager.h
@@ -10,21 +10,33 @@
 
 NS_ASSUME_NONNULL_BEGIN
 
+@protocol AvatarCloudListener <NSObject>
+@optional
+
+/**
+ * 失败信息
+ */
+- (void)avatarCloudError:(NSDictionary *)resultDic;
+
+@end
+
 @interface AvatarCloudSDKManager : NSObject
 
-//default 300
-@property (nonatomic, assign) CGFloat imageWidth;
 //default NO 设置YES时，cornerRadius无效
 @property (nonatomic, assign) BOOL isCircle;
 //default 24
 @property (nonatomic, assign) CGFloat cornerRadius;
+//default 60
+@property (nonatomic, assign) NSTimeInterval timeoutInterval;
 
-+ (void)initWithClient_id:(NSString *)client_id client_secret:(NSString *)client_secret;
++ (void)initWithClient_id:(NSString *)client_id client_secret:(NSString *)client_secret complete:(void(^_Nullable)(NSDictionary * _Nonnull resultDic))complete;
 
 + (nonnull instancetype)sharedInstance;
 - (void)initWithParentController:(id)viewController animated:(BOOL)animated;
 - (void)getImage:(void(^)(UIImage *image))imageBlock;
 - (void)getImageData:(void(^)(NSData *imageData))imageBlock;
+- (void)addAvatarCloudListener:(id<AvatarCloudListener>)listener;
+- (void)removeAvatarCloudListener;
 + (void)clearImageCache;
 
 @end
